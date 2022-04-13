ef7c291a7693307d01d3dede1f1b7e2bb1d8f38f
M.K <37064567+masatakashiwagi@users.noreply.github.com>
2022-04-14 01:04:06 +0900

    #7 add app file

---
+++ async-processing/app/main.py
@@ -0,0 +1,32 @@
+# import pika
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from logger import get_logger
+from producer import producer
+
+LOGGER = get_logger()
+
+# FastAPI instance
+app = FastAPI(title="FastAPI", version="1.0")
+
+# Middlewares
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Routes
+app.include_router(producer.router)
+# app.include_router(predict.router)
+
+
+# For Health Check
+@app.get("/")
+def health_check():
+    """Container Operation & Health Check
+    """
+    return {"Status": "OK"}
