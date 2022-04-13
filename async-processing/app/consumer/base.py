ea130d242ee0f2806355295fa8476fb873bc7566
M.K <37064567+masatakashiwagi@users.noreply.github.com>
2022-04-14 01:04:18 +0900

    #7 add consumer file

---
+++ async-processing/app/consumer/base.py
@@ -0,0 +1,182 @@
+import ast
+import os
+import pickle
+import sys
+import traceback
+from typing import Literal
+
+import boto3
+import numpy as np
+import pika
+from logger import get_logger
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+LOGGER = get_logger()
+
+# queue nameとして取り得る値
+QueueNames = Literal['queue.model.train', 'queue.model.predict']
+
+
+class BaseConsumer:
+    def __init__(self, queue_name: QueueNames):
+        self.queue_name = queue_name
+        self.pika_params = pika.ConnectionParameters(
+            host="rabbitmq",
+            port=os.getenv('RABBITMQ_PORT', 5672),
+            connection_attempts=10,
+            heartbeat=0
+        )
+        self.connection = pika.BlockingConnection(self.pika_params)
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=self.queue_name, auto_delete=False, durable=True)
+        self.channel.basic_qos(prefetch_count=1)
+        LOGGER.info('Pika connection initialized.')
+
+    def callback(self):
+        raise NotImplementedError()
+
+    def run(self):
+        """The consumer in standby mode
+        """
+        LOGGER.info(f" [*] Waiting for messages in {self.__class__.__name__}. To exit press CTRL+C.")
+        try:
+            self.channel.basic_consume(queue=self.queue_name, on_message_callback=self.callback, auto_ack=True)
+            self.channel.start_consuming()
+        except Exception as e:
+            _, _, tb = sys.exc_info()
+            LOGGER.error(
+                f'Exception Error: {e} || Type: {str(type(e))} || Traceback Message: {traceback.format_tb(tb)}')
+
+    @staticmethod
+    def body2dict(body: str):
+        """Convert body message to dict
+        """
+        body_str = body.decode("UTF-8")
+        body = ast.literal_eval(body_str)
+        LOGGER.info("Convert message to dict type.")
+
+        return body
+
+    @staticmethod
+    def download_from_s3(
+        s3_bucket_name: str,
+        s3_dir_path: str,
+        file_path: str,
+        file_name: str
+    ):
+        """Download from s3
+
+        Args:
+            s3_bucket_name (str): target bucket name
+            s3_dir_path (str): s3 directory path for download
+            file_path (str): local directory path
+            file_name (str): target filename
+        """
+        s3 = boto3.resource('s3')
+        bucket = s3.Bucket(s3_bucket_name)
+        s3_file_path = s3_dir_path + file_name
+
+        # workaround for bug
+        tmp_split_list = file_name.split('.csv')
+        if len(tmp_split_list) > 1:
+            new_file_name = file_name.split('.csv')[0] + '.csv'
+        else:
+            new_file_name = file_name.split('.csv')[0]
+        bucket.download_file(s3_file_path, file_path + new_file_name)
+
+    @staticmethod
+    def upload_to_s3(
+        s3_bucket_name: str,
+        s3_dir_path: str,
+        file_path: str,
+        file_name: str
+    ):
+        """Upload to s3
+
+        Args:
+            s3_bucket_name (str): target bucket name
+            s3_dir_path (str): s3 directory path for upload
+            file_path (str): local directory path
+            file_name (str): target filename
+        """
+        s3 = boto3.resource('s3')
+        bucket = s3.Bucket(s3_bucket_name)
+        s3_file_path = s3_dir_path + file_name
+        bucket.upload_file(file_path + file_name, s3_file_path)
+
+    @ staticmethod
+    def save_model(model: object, filename: str):
+        """Save trained model
+
+        Args:
+            model (object): model object
+            filename (str): model filename & filepath
+        """
+        with open(filename, mode='wb') as f:
+            pickle.dump(model, f)
+
+        return None
+
+    @staticmethod
+    def load_model(filename: str):
+        """Load ML model
+
+        Args:
+            filename (str): model filename & filepath
+        """
+        with open(filename, mode='rb') as f:
+            model = pickle.load(f)
+
+        return model
+
+
+class EvalMetrics(object):
+    """Model evaluation metrics
+    """
+
+    def __init__(self):
+        """initialize
+        """
+
+    def rmse_score(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        """Calculate Root Mean Square Error
+
+        Args:
+            y_true (np.ndarray): true y-values
+            y_pred (np.ndarray): pred y-values
+
+        Returns:
+            float: rmse values
+        """
+        mse = mean_squared_error(y_true, y_pred)
+        rmse = np.sqrt(mse)
+
+        return rmse
+
+    def mae_score(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        """Calculate Mean Absolute Error
+
+        Args:
+            y_true (np.ndarray): true y-values
+            y_pred (np.ndarray): pred y-values
+
+        Returns:
+            float: mae values
+        """
+        mae = mean_absolute_error(y_true, y_pred)
+
+        return mae
+
+    def r2_score(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        """Calculate R^2: coefficient of determination
+
+        Args:
+            y_true (np.ndarray): true y-values
+            y_pred (np.ndarray): pred y-values
+
+        Returns:
+            float: mae values
+        """
+        r2 = r2_score(y_true, y_pred)
+
+        return r2
---
+++ async-processing/app/consumer/tasks.py
@@ -0,0 +1,244 @@
+import json
+import os
+import sys
+import traceback
+from typing import Any, Dict
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pika
+from logger import get_logger
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+
+from base import BaseConsumer, EvalMetrics, QueueNames
+
+LOGGER = get_logger()
+S3_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
+S3_PATH_NAME = os.environ['S3_PATH_NAME']
+S3_MODEL_PATH_NAME = os.environ['S3_MODEL_PATH_NAME']
+
+
+class TrainConsumer(BaseConsumer):
+    def __init__(self, queue_name: QueueNames):
+        BaseConsumer.__init__(self, queue_name)
+
+    def callback(self, ch, method, props, body):
+        params = self.body2dict(body)
+        self.download_from_s3(S3_BUCKET_NAME, S3_PATH_NAME, 'data/', params['dataset_id'] + '.csv')
+        LOGGER.info("Download dataset from S3.")
+
+        dataset_path = 'data/' + params['dataset_id'] + '.csv'
+        df = pd.read_csv(dataset_path)
+        LOGGER.info("Read csv file and transform to dataframe.")
+
+        try:
+            _ = self.train(df, params)
+            model_id = params['model_id']
+            self.upload_to_s3(S3_BUCKET_NAME, S3_MODEL_PATH_NAME + f'{model_id}/', 'data/', 'model.pkl')
+            LOGGER.info("Upload trained model to S3.")
+
+            payload = {
+                'status': 'TASK_COMPLETED',
+                'model_id': params['model_id']
+            }
+            response = json.dumps(payload)
+        except Exception as e:
+            _, _, tb = sys.exc_info()
+            LOGGER.error(
+                f'Exception Error: {e} || Type: {str(type(e))} || Traceback Message: {traceback.format_tb(tb)}')
+
+            payload = {
+                'status': 'TASK_ERROR',
+                'model_id': params['model_id']
+            }
+            response = json.dumps(payload)
+
+        ch.basic_publish(
+            exchange='',
+            routing_key=props.reply_to,
+            properties=pika.BasicProperties(correlation_id=props.correlation_id),
+            body=response
+        )
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    def train(self, df: pd.DataFrame, params) -> Dict[str, Any]:
+        """Train machine learning model
+
+        Args:
+            df (pd.DataFrame): dataset for training model
+            params (dict): parameters for training
+        """
+        features = params['features']
+        target = params['target']
+        X, y = df[features], df[target].values
+
+        # train/test split
+        X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=42)
+
+        LOGGER.info("Start model training.")
+        # machine learning model: RandomForestRegressor
+        reg_model = RandomForestRegressor(max_depth=3, random_state=42, n_estimators=100)
+        reg_model.fit(X_train, y_train)
+        LOGGER.info("Model fit for training.")
+
+        # save model
+        model_path = 'data/model.pkl'
+        self.save_model(reg_model, model_path)
+        LOGGER.info("Save trained model to local.")
+
+        # evaluate model
+        pred = reg_model.predict(X_valid)
+
+        # evaluate metrics
+        eval_metrics = EvalMetrics()
+        rmse = eval_metrics.rmse_score(y_valid, pred)
+        LOGGER.info("Evaluate metrics=RMSE for valid dataset : %.3f" % rmse)
+        LOGGER.info("Finish model training.")
+
+        result = {
+            'y_pred': pred,
+            'y_true': y_valid,
+            'metrics': {'rmse': rmse}
+        }
+
+        return result
+
+
+class PredictConsumer(BaseConsumer):
+    def __init__(self, queue_name: QueueNames):
+        BaseConsumer.__init__(self, queue_name)
+
+    def callback(self, ch, method, props, body):
+        params = self.body2dict(body)
+        model_id = params['model_id']
+        self.download_from_s3(S3_BUCKET_NAME, S3_MODEL_PATH_NAME + f'{model_id}/', 'data/', 'model.pkl')
+        LOGGER.info("Download model file from S3.")
+
+        model_path = 'data/model.pkl'
+        model = self.load_model(model_path)
+        LOGGER.info("Load model for prediction.")
+
+        try:
+            result = self.predict(model, params)
+
+            payload = {
+                'status': 'TASK_COMPLETED',
+                'pred_proba': result['pred_proba']
+            }
+            response = json.dumps(payload)
+        except Exception as e:
+            _, _, tb = sys.exc_info()
+            LOGGER.error(
+                f'Exception Error: {e} || Type: {str(type(e))} || Traceback Message: {traceback.format_tb(tb)}')
+
+            payload = {
+                'status': 'TASK_ERROR',
+                'pred_proba': None
+            }
+            response = json.dumps(payload)
+
+        ch.basic_publish(
+            exchange='',
+            routing_key=props.reply_to,
+            properties=pika.BasicProperties(correlation_id=props.correlation_id),
+            body=response
+        )
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    def predict(self, model: object, params) -> Dict[str, Any]:
+        """Prediction for dataset using trained model
+
+        Args:
+            model (object): trained model
+            params (_type_): parameters for prediction
+
+        Returns:
+            float: predict probability
+        """
+        input_data = params['input_data']
+        pred_proba = model.predict(pd.DataFrame([input_data]))
+        result = {
+            'pred_proba': pred_proba[0]
+        }
+
+        return result
+
+
+if __name__ == "__main__":
+    def plot_yy(y_valid, y_pred, metrics, savepath):
+        """yy-plotで結果を可視化
+        """
+        y_max = np.max(y_valid)
+        y_min = np.min(y_valid)
+
+        # y_predの最大値、最小値を計算する。
+        predict_y_max = np.max(y_pred)
+        predict_y_min = np.min(y_pred)
+
+        # 全てのプロットが収まるようにするには、y_validとy_pred両方のうち
+        # 最も小さい値、最も大きい値を縦軸横軸の範囲にすればいい。
+        axis_max = max(y_max, predict_y_max)
+        axis_min = min(y_min, predict_y_min)
+
+        # プロットエリアの長さは縦横ともにaxis_max-axis_min。
+        # これの5%の長さ分の余白を取る。
+        axis_max = axis_max + (axis_max - axis_min) * 0.05
+        axis_min = axis_min - (axis_max - axis_min) * 0.05
+
+        plt.figure(figsize=(10, 6))
+        plt.subplots_adjust(wspace=0.2, hspace=0.3)
+        plt.scatter(y_pred, y_valid, c='r', s=50, zorder=2, edgecolors=(0, 0, 0), alpha=0.6)
+        plt.plot([axis_min, axis_max], [axis_min, axis_max], c="#1560bd")
+
+        plt.xlabel('Predict Values', fontsize=20)
+        plt.ylabel('True Values', fontsize=20)
+        plt.title(r'RMSE=%.2f' % (metrics), fontsize=15)
+        plt.tick_params(labelsize=20)
+        plt.tight_layout()
+        plt.grid(True)
+        plt.savefig(savepath, dpi=100, bbox_inches='tight', pad_inches=0.1)
+        plt.close()
+
+    def plot_residual(y_valid, y_pred, savepath):
+        residual = y_pred - y_valid
+        xmax = np.max(y_pred) + (np.max(y_pred) - np.min(y_pred)) * 0.05
+        xmin = np.min(y_pred) - (np.max(y_pred) - np.min(y_pred)) * 0.05
+
+        plt.figure(figsize=(10, 6))
+        plt.subplots_adjust(wspace=0.2, hspace=0.3)
+        plt.scatter(y_pred, residual, c='r', s=50, zorder=2, edgecolors=(0, 0, 0), alpha=0.6)
+        plt.hlines(y=0, xmin=xmin, xmax=xmax, color='#1560bd')
+        plt.title('Residual Plot', fontsize=20)
+        plt.xlabel('Predict Values', fontsize=20)
+        plt.ylabel('Residuals', fontsize=20)
+        plt.tick_params(labelsize=20)
+        plt.tight_layout()
+        plt.grid(True)
+        plt.savefig(savepath, dpi=100, bbox_inches='tight', pad_inches=0.1)
+        plt.close()
+
+    params = {
+        "model_id": "sample_test",
+        "dataset_id": "test_diabetes",
+        "features": ["age", "bmi", "bp", "s1", "s2", "s3", "s4", "s5", "s6"],
+        "target": "target"
+    }
+    dataset_path = 'test/' + params['dataset_id'] + '.csv'
+    df = pd.read_csv(dataset_path)
+    result = TrainConsumer(queue_name='queue.model.train').train(df, params)
+
+    rmse = result['metrics']['rmse']
+
+    # Record the metrics
+    with open("data/metrics.txt", "w") as outfile:
+        outfile.write("RMSE: " + f"{rmse:.2f}" + "\n")
+
+    # Plot results
+    y_valid = result['y_true']
+    y_pred = result['y_pred']
+    savepath_yy = "data/yy_plot.png"
+    plot_yy(y_valid, y_pred, metrics=rmse, savepath=savepath_yy)
+    savepath_residual = "data/residual_plot.png"
+    plot_residual(y_valid, y_pred, savepath=savepath_residual)
