c4001ff74674fa274d2af79621cd5d86140809e8
M.K <37064567+masatakashiwagi@users.noreply.github.com>
2022-04-28 12:20:58 +0900

    Fix thread executor for consumer.py

--- async-processing/app/consumer/consumer.py
+++ async-processing/app/consumer/consumer.py
@@ -1,4 +1,4 @@
-import threading
+from concurrent.futures import ThreadPoolExecutor
 
 import click
 
@@ -6,21 +6,17 @@ import tasks
 
 
 @click.command()
-@click.option("--num_threads", type=int, help='the number of threads', required=True, default=2)
-def main(num_threads):
+@click.option("--num_threads", type=int, help='the number of threads', default=1)
+@click.option("--max_workers", type=int, help='the number of max workers', default=None)
+def main(num_threads: int, max_workers: int):
     # Consumer execution
-    threads = []
-    for _ in range(num_threads):
-        for task in [
-            tasks.TrainConsumer(queue_name='queue.model.train'),
-            tasks.PredictConsumer(queue_name='queue.model.predict')
-        ]:
-            t = threading.Thread(target=task.run, daemon=True)
-            t.start()
-            threads.append(t)
-
-    for t in threads:
-        t.join()
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        for _ in range(num_threads):
+            for task in [
+                tasks.TrainConsumer(queue_name='queue.model.train'),
+                tasks.PredictConsumer(queue_name='queue.model.predict')
+            ]:
+                executor.submit(task.run)
 
 
 if __name__ == "__main__":
